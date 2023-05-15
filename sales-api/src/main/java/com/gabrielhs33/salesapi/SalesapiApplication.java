0e135ae1d10c8a7746c4e254605a2621c805d94f
Gabriel <ev04hs@gmail.com>
2023-05-15 09:53:58 -0300

    create spring project

---
+++ sales-api/src/main/java/com/gabrielhs33/salesapi/SalesapiApplication.java
@@ -0,0 +1,13 @@
+package com.gabrielhs33.salesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesapiApplication.class, args);
+	}
+
+}
---
+++ sales-api/src/test/java/com/gabrielhs33/salesapi/SalesapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrielhs33.salesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
