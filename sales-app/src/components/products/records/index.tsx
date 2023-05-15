0bc9f74d3b463c11dc3987ea41b20336ccf1a843
Gabriel <ev04hs@gmail.com>
2023-05-14 21:27:42 -0300

    Manipulating Forms with State

--- sales-app/src/components/products/records/index.tsx
+++ sales-app/src/components/products/records/index.tsx
@@ -1,48 +1,69 @@
+import { useState } from 'react'
 import { Layout }  from 'components'
 
 export const RegisterProducts: React.FC = () =>{
+
+    const [ sku, setSku ] = useState('')
+    const [ price, setPrice ] = useState('')
+    const [ name, setName ] = useState('')
+    const [ description, setDescription ] = useState('')
+
+    const submit = () => {
+
+        const product = {
+
+            sku,
+            price,
+            name,
+            description
+        }
+        console.log(product)
+    }
 
     return(
 
         <Layout title="Products">
 
-            <div className="field">
-                <label className="label" htmlFor="inputSku">SKU: *</label>
-                <div className="control">
-                    <input className="input" 
-                            id="inputSku"
-                            placeholder="Enter product SKU"/>
+            <div className="columns">
+                <div className="field is-half column">
+                    <label className="label" htmlFor="inputSku">SKU: *</label>
+                    <div className="control">
+                        <input className="input" 
+                                id="inputSku" value={sku} onChange={event => setSku(event.target.value)}
+                                placeholder="Enter product SKU"/>
+                    </div>
+                </div>
+                <div className="field is-half column">
+                    <label className="label" htmlFor="inputPrice">Price: </label>
+                    <div className="control">
+                        <input className="input" 
+                                id="inputPrice" value={price} onChange={event => setPrice(event.target.value)}
+                                placeholder="Enter product Price"/>
+                    </div>
                 </div>
             </div>
-            <div className="field">
-                <label className="label" htmlFor="inputPrice">Price: </label>
-                <div className="control">
-                    <input className="input" 
-                            id="inputPrice"
-                            placeholder="Enter product Price"/>
-                </div>
-            </div>
-            <div className="field">
+         
+            <div className="field column is-full">
                 <label className="label" htmlFor="inputSku">Name: </label>
                 <div className="control">
                     <input className="input" 
-                            id="inputName"
+                            id="inputName" value={name} onChange={event => setName(event.target.value)}
                             placeholder="Enter product Name"/>
                 </div>
             </div>
 
-            <div className="field">
+            <div className="field column is-full">
                 <label className="label" htmlFor="inputSku">Description: </label>
                 <div className="control">
                     <textarea className="textarea" 
-                            id="inputDesc"
+                            id="inputDesc" value={description} onChange={event => setDescription(event.target.value)}
                             placeholder="Enter product Description"/>
                 </div>
             </div>
 
             <div className="field is-grouped">
                 <div className="control">
-                    <button className="button is-link">Submit</button>
+                    <button onClick={submit} className="button is-link">Submit</button>
                 </div>
                 <div className="control">
                     <button className="button is-link is-light">Turn Back</button>
@@ -51,3 +72,4 @@ export const RegisterProducts: React.FC = () =>{
         </Layout>
     )
 }
+
